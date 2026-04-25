31016e04d4016175cef1f99b3e0a722ca2e8b0a5
Ahmed Alhamadi <devahmedabduallah@gmail.com>
2026-04-25 15:47:45 +0300

    fix: allow all origins in socket cors to prevent vercel connection issues

--- socketManager.js
+++ socketManager.js
@@ -15,7 +15,7 @@ function initSocket(server) {
   
   io = new Server(server, {
     cors: {
-      origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:3001'],
+      origin: '*', // Allow all origins including Vercel
 
       methods: ['GET', 'POST'],
       credentials: true,
